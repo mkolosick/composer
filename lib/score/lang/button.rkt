bf413e7f59ba675aab9854c1e977e70e56507fd3
AlexKnauth <alexander@knauth.org>
2018-04-06 17:24:19 -0400

    Render Score button gets the `score` value from the module

--- lib/score/lang/button.rkt
+++ lib/score/lang/button.rkt
@@ -4,17 +4,62 @@
 
 (require racket/class
          racket/draw
-         racket/runtime-path)
+         racket/runtime-path
+         racket/lazy-require)
+(lazy-require
+ ["gui.rkt"
+  (execute-callback-with-rep:after-expression
+   message-box)])
 
 ;; render-score.png points to a small image for the button
 (define-runtime-path render-score-icon.png "render-score-icon.png")
 
 ;; ------------------------------------------------------------------------
 
+;; file-render-score : Path Score -> Void
+
+;; file  : a path representing the source file that the score comes from
+;; score : the score value provided by the module as `score`
+
+;; Renders the `score` to a different file with a path relative to `file`.
+(define (file-render-score file score)
+  ;; TODO: render the score
+  (void))
+
+;; ------------------------------------------------------------------------
+
 ;; render-score-action : DrRacket:Unit:Frame -> Void
 ;; ASSUME this is called within the DrRacket eventspace
 (define (render-score-action frame)
-  (void))
+  ;; this "Render Score" button action is based on Scribble's
+  ;; "Render Scribble" button actions, from
+  ;; drracket/scribble/tools/drracket-buttons.rkt
+  (define dtext (send frame get-definitions-text))
+  (define filename (send dtext get-filename))
+  (cond
+    [filename
+     (execute-callback-with-rep:after-expression
+      frame
+      (λ ()
+        ;; This thunk will be evaluated in the namespace of
+        ;; the module, just like the Repl.
+        (define score
+          (or
+           (with-handlers ([exn:fail? (λ (x) #f)])
+             (dynamic-require (eval #'(variable-reference->module-path-index
+                                       (#%variable-reference)))
+                              'score))
+           (with-handlers ([exn:fail? (λ (x) #f)])
+             (eval 'score))))
+        ;; If (dynamic-require ... 'score) and (eval 'score) both go wrong,
+        ;; then we assume that's because of an earlier failure, so we just
+        ;; don't do anything.
+        (when score
+          (printf "rendering score\n")
+          ;; Render the score here
+          (file-render-score filename score))))]
+    [else
+     (message-box "Music" "Cannot render a music score without a filename")]))
 
 ;; ------------------------------------------------------------------------
 
